955702f30162845e84b0d5e84693868073f4bdd0
Eder <edermunhozsantos@gmail.com>
2020-02-04 01:03:28 -0300

    feat: update googleMap component with lat lng props

--- src/components/google-map/GoogleMap.js
+++ src/components/google-map/GoogleMap.js
@@ -2,14 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
 
-const GoogleMap = ({ google }) => (
-  <Map google={google} zoom={10}>
-    <Marker title='Minha localização' name='location' />
+const GoogleMap = ({ google, lat = -23.5168338, lng = -46.6139824 }) => (
+  <Map
+    google={google}
+    zoom={16}
+    initialCenter={{ lat, lng }}
+    center={{ lat, lng }}
+  >
+    <Marker title='Minha localização' name='location' position={{ lat, lng }} />
   </Map>
 )
 
 GoogleMap.propTypes = {
-  google: PropTypes.object
+  google: PropTypes.object,
+  lat: PropTypes.number,
+  lng: PropTypes.number,
+  geoCode: PropTypes.object
 }
 
 export default GoogleApiWrapper({
